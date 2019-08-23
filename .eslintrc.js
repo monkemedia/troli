bd40ebea05935d0292ba450bfbd4fb608b6b75cb
monkemedia <info@monkemedia.co.uk>
2019-08-23 05:27:11 +0100

    Updated eslint rule to force indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
       'singleline': 'never',
       'multiline': 'never'
     }],
+    'indent': 'off',
+    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
