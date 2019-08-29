d539a32e14a45566cf8903398dd01708436d05fa
monkemedia <info@monkemedia.co.uk>
2019-08-29 20:10:11 +0100

    We now have locale in html lang meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,11 +6,26 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
   import Navbar from '~/components/Navbar.vue'
 
   export default {
     components: {
       Navbar
+    },
+
+    head () {
+      return {
+        htmlAttrs: {
+          lang: this.locale
+        }
+      }
+    },
+
+    computed: {
+      ...mapGetters({
+        locale: 'locales/getLocale'
+      })
     }
   }
 </script>
