78bc4444c0c65a15e14024f29cc9d32f6d56602f
monkemedia <info@monkemedia.co.uk>
2019-09-05 17:32:17 +0100

    Added accessibility addon to Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
-import { configure } from '@storybook/vue'
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { configure, addDecorator } from '@storybook/vue'
+import { withA11y } from '@storybook/addon-a11y'
 
 import 'bulma'
 // import '@fortawesome/fontawesome-free/css/all.css'
 import '../assets/scss/main.scss'
+
+addDecorator(withA11y)
 
 // Components
 // import B from '../components/LanguageSelector.vue'
--- components/stories/Buttons.stories.js
+++ components/stories/Buttons.stories.js
@@ -1,10 +1,14 @@
 import { storiesOf } from '@storybook/vue'
+import ButtonDefault from '../ButtonDefault.vue'
 
 storiesOf('Buttons', module)
   .add('Primary', () => ({
+    components: {
+      ButtonDefault
+    },
     template: `
         <div class="container">
-          <button class="button is-large">Text</button>
+          <button-default is-flip text="Create a new account"></button-default>
         </div>
     `
   }))
